73d8256dc584019ce0e3512287d662de74a1bd7f
Elias <eliasbrucart@hardgames.com.ar>
2021-06-28 13:11:58 -0300

    Added levels

---
+++ Assets/Scripts/Gameplay/Level/LevelManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public List<GameObject> levels = new List<GameObject>();
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
