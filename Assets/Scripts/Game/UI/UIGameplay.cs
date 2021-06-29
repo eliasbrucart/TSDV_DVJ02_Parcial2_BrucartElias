b3eb480bcdb270f9df41634ab0c21fca44147f1f
Elias <eliasbrucart@hardgames.com.ar>
2021-06-29 18:41:22 -0300

    UI in gameplay

--- Assets/Scripts/Game/UI/UIGameplay.cs
+++ Assets/Scripts/Game/UI/UIGameplay.cs
@@ -1,7 +1,15 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 
 public class UIGameplay : MonoBehaviour
 {
+    public TMP_Text scoreText;
+    public TMP_Text timeText;
+    public TMP_Text fuelText;
+    public TMP_Text altitudeText;
+    public TMP_Text verticalSpeedText;
+    public TMP_Text horizontalSpeedText;
+
     void Start()
     {
         
