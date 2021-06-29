54b79357112d6970d22a9f3a60ae24c87aed63a3
Elias <eliasbrucart@hardgames.com.ar>
2021-06-29 17:57:13 -0300

    GameOver screen works

---
+++ Assets/Scripts/Game/UI/UIGameOver.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIGameOver : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void PlayAgain()
+    {
+        ScenesManager.instanceScenesManager.UnloadSceneAsy("GameOver");
+        GameManager.instanceGameManager.ResetGamePlay();
+        Time.timeScale = 1;
+    }
+
+    public void MainMenuBtn()
+    {
+        ScenesManager.instanceScenesManager.UnloadSceneAsy("GameOver");
+        ScenesManager.instanceScenesManager.ChangeScene("MainMenu");
+        Time.timeScale = 1;
+    }
+
+    public void QuitBtn()
+    {
+        ScenesManager.instanceScenesManager.OnClickQuit();
+    }
+}
