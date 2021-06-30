481673b0fde8386a8530a6b63b1b7cfea5ec91f3
Elias <eliasbrucart@hardgames.com.ar>
2021-06-30 03:52:50 -0300

    Added splash screen

--- Assets/Scripts/SplashScreen/SplashScreenManager.cs
+++ Assets/Scripts/SplashScreen/SplashScreenManager.cs
@@ -4,13 +4,70 @@ using UnityEngine;
 
 public class SplashScreenManager : MonoBehaviour
 {
+    [SerializeField] private CanvasGroup gameSplash;
+    [SerializeField] private CanvasGroup companySplash;
+    [SerializeField] private float maxTime = 2.0f;
+    private float timer;
+    private ScenesManager sm;
     void Start()
     {
-        
+        gameSplash.alpha = 0;
+        companySplash.alpha = 0;
+        StartCoroutine(Splash());
+        sm = ScenesManager.instanceScenesManager;
     }
 
-    void Update()
+    IEnumerator Splash()
     {
-        
+        while (timer < maxTime)
+        {
+            SetAlpha1(companySplash);
+            yield return null;
+        }
+        timer = 0;
+        yield return new WaitForSeconds(0.5f);
+
+        while (timer < maxTime)
+        {
+            SetAlpha0(companySplash);
+            yield return null;
+        }
+        timer = 0;
+        yield return new WaitForSeconds(0.1f);
+
+        while (timer < maxTime)
+        {
+            SetAlpha1(gameSplash);
+            yield return null;
+        }
+        timer = 0;
+        yield return new WaitForSeconds(0.5f);
+
+        while (timer < maxTime)
+        {
+            SetAlpha0(gameSplash);
+            yield return null;
+        }
+        timer = 0;
+        LoadMainMenu();
     }
+    
+    private void LoadMainMenu()
+    {
+        sm.ChangeScene("MainMenu");
+    }
+
+    private void SetAlpha1(CanvasGroup canvasG)
+    {
+        timer += Time.deltaTime;
+        canvasG.alpha = Mathf.Lerp(0, 1, timer);
+    }
+
+    private void SetAlpha0(CanvasGroup canvasG)
+    {
+        timer += Time.deltaTime;
+        canvasG.alpha = Mathf.Lerp(1, 0, timer);
+    }
+
+
 }
